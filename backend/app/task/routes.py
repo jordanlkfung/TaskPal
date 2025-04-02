5f5dde58d4cdbbabe07ae4675d4aaa18d2e692f8
jordanlkfung <jordanlkfung@gmail.com>
2025-04-02 11:19:06 -0700

    delete and update work

--- backend/app/task/routes.py
+++ backend/app/task/routes.py
@@ -8,14 +8,14 @@ task_router = APIRouter()
 task_service = TaskService()
 
 @task_router.get('/{collection_id}', status_code=status.HTTP_200_OK)
-async def getTasks(collection_id:int, db: AsyncSession = Depends(get_db)):
+async def getTasks(collection_id:int, db: AsyncSession = Depends(get_db), sort: str = "none"):
     return await task_service.getTasks(collection_id, db)
 
 @task_router.post('/add', status_code=status.HTTP_201_CREATED)
 async def addTask(newTask: addTaskSchema, db: AsyncSession = Depends(get_db)):
     return await task_service.addTask(newTask, db)
 
-@task_router.patch('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@task_router.patch('/', status_code=status.HTTP_204_NO_CONTENT)
 async def updateTask(task:updateTaskSchema, db:AsyncSession = Depends(get_db)):
     await task_service.updateTask(task, db)
 
@@ -24,6 +24,7 @@ async def deleteTask(id: int, db: AsyncSession = Depends(get_db)):
     await task_service.deleteTask(id, db)
 
 #keep or not keep, patch gives same functionality
+#keep for now, allows update of task without sending anything in body
 @task_router.patch("/complete/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def completeTask(id: int, db:AsyncSession = Depends(get_db)):
     await task_service.completeTask(id, db)
