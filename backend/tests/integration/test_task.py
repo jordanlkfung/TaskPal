27c42f8455d4d005cb8edc2d839bd9fe57af1eba
jordanlkfung <jordanlkfung@gmail.com>
2025-04-24 13:57:35 -0700

    changes to task schema body request

--- backend/tests/integration/test_task.py
+++ backend/tests/integration/test_task.py
@@ -81,5 +81,78 @@ async def test_create_task_missing_token(client, get_invalid_auth_heater):
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 @pytest.mark.asyncio
-async def test_update_task(client, create_test_task):
+async def test_update_task(client, create_test_task, get_auth_header_for_user):
+    response = await client.patch(f'task/',
+                                  headers = {"Authorization":get_auth_header_for_user},
+                                  json={
+                                    "name": "string",
+                                    "priority": 0,
+                                    "id": create_test_task.id
+                                    })
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+@pytest.mark.asyncio
+async def test_update_task_invalid_priority(client, create_test_task, get_auth_header_for_user):
+    response = await client.patch(f'task/',
+                                  headers = {"Authorization":get_auth_header_for_user},
+                                  json={
+                                    "name": "string",
+                                    "priority": 10,
+                                    "id": create_test_task.id
+                                    })
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.asyncio
+async def test_update_task_invalid_task_id(client, create_test_task, get_auth_header_for_user):
+    response = await client.patch(f'task/',
+                                  headers = {"Authorization":get_auth_header_for_user},
+                                  json={
+                                    "name": "string",
+                                    "priority": 10,
+                                    "id": 100
+                                    })
+    assert response.status_code == status.HTTP_404_NOT_FOUND 
+@pytest.mark.asyncio
+async def test_update_task_unauthorized_user(client,create_test_task, get_auth_header_for_second_user):
+    response = await client.patch('task/',
+                                  headers = {"Authorization":get_auth_header_for_second_user},
+                                  json={
+                                    "name": "string",
+                                    "priority": 1,
+                                    "id": create_test_task.id
+                                    })
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+@pytest.mark.asyncio
+async def test_update_task_invalid_token(client, get_invalid_auth_heater):
+    response = await client.patch('task/',
+                                  headers = {"Authorization":get_invalid_auth_heater},
+                                  json={
+                                    "name": "string",
+                                    "priority": 1,
+                                    "id": 0
+                                    })
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+@pytest.mark.asyncio
+async def test_delete_task(client,create_test_task,get_auth_header_for_user):
+    response = await client.delete(f"/task/{create_test_task.id}",
+                                   headers={"Authorization": get_auth_header_for_user})
+    
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+@pytest.mark.asyncio
+async def test_delete_task_missing_headers():
+    pass
+
+@pytest.mark.asyncio
+async def test_delete_task_task_does_not_exist():
+    pass
+
+@pytest.mark.asyncio
+async def test_delete_task_unauthorized_user():
+    pass
+
+@pytest.mark.asyncio
+async def test_delete_task_invalid_token():
     pass
