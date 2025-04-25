8dc2992f6de3360741d507a0365362e45135c4c5
jordanlkfung <jordanlkfung@gmail.com>
2025-04-24 23:19:23 -0700

    migrated to sqlalchemy mapped columns

--- backend/app/users/model.py
+++ backend/app/users/model.py
@@ -1,15 +1,15 @@
 from typing import List, TYPE_CHECKING
-from sqlalchemy import Column, String, Integer, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, mapped_column, Mapped
 import datetime
 from app.database import Base
-if TYPE_CHECKING:
+if TYPE_CHECKING: #type checking needed to pass flake test
     from app.collection.model import Collection
 
 class User(Base):
     __tablename__ = "users"
-    id= Column(Integer, primary_key=True)
-    email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
-    date_created = Column(DateTime, default =datetime.datetime.now)
-    collection = relationship('Collection', back_populates='collectionOwner')
+    id:Mapped[int] = mapped_column(primary_key=True)
+    email:Mapped[str] = mapped_column(unique=True)
+    password:Mapped[str]
+    date_created:Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now)
+    collection:Mapped[List['Collection']] = relationship(back_populates='collectionOwner')
+
